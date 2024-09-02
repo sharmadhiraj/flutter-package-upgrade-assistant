1a048c73d2d85c049b63ad1da369d610b90af996
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2024-09-02 14:55:10 +0545

    Handle local and git repo in dependency, plus minor improvements

--- main.dart
+++ main.dart
@@ -3,115 +3,120 @@ import 'dart:io';
 
 import 'package.dart';
 
-late File file;
+late File pubspecFile;
 
 void main(List<String> args) async {
-  initFile(args);
-  final List<Package> packages = getPackages();
+  initializePubspecFile(args);
+  final List<Package> packages = extractPackagesFromPubspec();
   final List<Package> updatedPackages =
-      await getLatestVersionForPackages(packages);
-  await updatePackageVersionsInPubspecFile(updatedPackages);
-  print("Packages upgrade complete. Thanks for using. Please rate on GitHub.");
+      await fetchLatestVersionsForPackages(packages);
+  await updatePubspecFileWithNewVersions(updatedPackages);
+  print(
+      "Package upgrade process completed successfully. Thank you for using the Flutter Package Upgrade Assistant. Please consider rating it on GitHub.");
   print("https://github.com/sharmadhiraj/flutter-package-upgrade-assistant");
 }
 
-List<Package> getPackages() {
-  final String dependenciesSection = file
+List<Package> extractPackagesFromPubspec() {
+  final String dependenciesSection = pubspecFile
       .readAsStringSync()
       .split(RegExp(r'dependencies:|dev_dependencies:'))[1];
   final List<String> dependencies = dependenciesSection.split("\n");
   final List<Package> packages = [];
-  dependencies.forEach((orgItem) {
-    final String item = orgItem.replaceAll(" ", "");
-    if (item.isEmpty || item == "flutter:" || item == "sdk:flutter") return;
-    final List<String> itemSplit = item.split(":");
-    if (itemSplit.length != 2) return;
+  dependencies.forEach((dependency) {
+    final String cleanedDependency = dependency.trim();
+    if (cleanedDependency.isEmpty ||
+        cleanedDependency == "flutter:" ||
+        cleanedDependency == "sdk: flutter" ||
+        !RegExp(r'^[a-z0-9_]+: .+$').hasMatch(cleanedDependency)) return;
+    final List<String> dependencyParts = cleanedDependency.split(":");
+    if (dependencyParts.length != 2) return;
     packages.add(
       Package(
-        raw: orgItem,
-        name: itemSplit.first,
-        version: itemSplit[1].replaceAll("^", ""),
+        raw: dependency,
+        name: dependencyParts.first,
+        version: dependencyParts[1].replaceAll("^", "").trim(),
       ),
     );
   });
-  print("Retrieved ${packages.length} packages from pubspec.yaml");
+  print("Found ${packages.length} packages in pubspec.yaml.");
   return packages;
 }
 
-Future<String?> getLatestVersionForPackage(String packageName) async {
-  final url = Uri.parse("https://pub.dev/packages/$packageName");
-  final client = HttpClient();
+Future<String?> fetchLatestVersionForPackage(String packageName) async {
+  print("Checking for the latest version of the package: $packageName");
+  final Uri url = Uri.parse("https://pub.dev/packages/$packageName");
+  final HttpClient client = HttpClient();
   try {
-    final request = await client.getUrl(url);
-    final response = await request.close();
+    final HttpClientRequest request = await client.getUrl(url);
+    final HttpClientResponse response = await request.close();
 
     if (response.statusCode == HttpStatus.ok) {
       String content = await utf8.decodeStream(response);
       final int startIndex = content.indexOf('class="title') + 14;
       final int endIndex = content.indexOf("span ", startIndex) - 2;
       content = content.substring(startIndex, endIndex);
-      content = content.replaceAll("$packageName ", "");
+      content = content.replaceAll("$packageName ", "").trim();
+      print("Latest version of $packageName retrieved: $content");
       return content;
     } else {
-      print("Failed to fetch data. Status code: ${response.statusCode}");
+      print(
+          "Failed to fetch the latest version for $packageName. Status code: ${response.statusCode}");
     }
   } catch (error) {
-    print("Error: $error");
+    print(
+        "An error occurred while fetching the latest version for $packageName: $error");
   } finally {
     client.close();
   }
   return null;
 }
 
-Future<List<Package>> getLatestVersionForPackages(
+Future<List<Package>> fetchLatestVersionsForPackages(
     List<Package> packages) async {
   final List<Package> updatedPackages = [];
-  for (int i = 0; i < packages.length; i++) {
-    final Package package = packages[i];
-    final String? newVersion = await getLatestVersionForPackage(package.name);
-    package.newVersion = newVersion;
+  for (Package package in packages) {
+    final String? latestVersion =
+        await fetchLatestVersionForPackage(package.name);
+    package.newVersion = latestVersion;
     updatedPackages.add(package);
   }
   print(
-    "Got ${updatedPackages.length} packages from pub.dev, ${updatedPackages.where((item) => item.hasNewVersion()).length} have updates.",
+    "Checked ${updatedPackages.length} packages on pub.dev. Found updates for ${updatedPackages.where((pkg) => pkg.hasNewVersion()).length} packages.",
   );
   return updatedPackages;
 }
 
-Future<void> updatePackageVersionsInPubspecFile(
+Future<void> updatePubspecFileWithNewVersions(
     List<Package> updatedPackages) async {
-  String pubspecFileContent = file.readAsStringSync();
-  updatedPackages.forEach((package) {
+  String pubspecContent = pubspecFile.readAsStringSync();
+  for (Package package in updatedPackages) {
     if (package.hasNewVersion()) {
       print(
-        "${package.name} package updated to ${package.newVersion} from ${package.version}",
-      );
-      pubspecFileContent = pubspecFileContent.replaceAll(
+          "Updating ${package.name} from version ${package.version} to ${package.newVersion}");
+      pubspecContent = pubspecContent.replaceAll(
         package.raw,
         package.getNewVersionRaw(),
       );
     }
-  });
-  await file.writeAsString(pubspecFileContent);
-  print("pubspec.yaml updated with new package versions.");
+  }
+  await pubspecFile.writeAsString(pubspecContent);
+  print("pubspec.yaml updated with the latest package versions.");
 }
 
-void initFile(List<String> arguments) {
+void initializePubspecFile(List<String> args) {
   String filePath = "pubspec.yaml";
-  if (arguments.length > 0) {
-    filePath = arguments.first;
+  if (args.isNotEmpty) {
+    filePath = args.first;
     if (!filePath.endsWith("pubspec.yaml")) {
       filePath = "$filePath/pubspec.yaml".replaceAll("//", "/");
     }
-    print("Using provided arguments as filepath for pubspec.yaml: $filePath");
+    print("Using specified path for pubspec.yaml: $filePath");
   } else {
-    print(
-      "No arguments provided, assuming pubspec.yaml exists in the current directory",
-    );
+    print("No path specified. Using the current directory for pubspec.yaml.");
   }
-  file = File(filePath);
-  if (!file.existsSync()) {
-    print("File does not exists on given path : $filePath");
+  pubspecFile = File(filePath);
+  if (!pubspecFile.existsSync()) {
+    print("pubspec.yaml file not found at the specified path: $filePath");
     exit(0);
   }
 }
